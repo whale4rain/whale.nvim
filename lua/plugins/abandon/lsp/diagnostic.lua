95a985c59828479f0935c47f8b7ce18e6502580e
whale4rain <panyucheng0123@icloud.com>
2025-11-28 02:09:16 +0800

    remove: remove the abandon fold

--- lua/plugins/abandon/lsp/diagnostic.lua
+++
@@ -1,6 +0,0 @@
--- vim.diagnostic.config({
--- 	virtual_text = false,
--- 	virtual_lines = { only_current_line = true },
--- 	float = { boarder = "rounded" },
--- 	underline = true, --启用下划线
--- })
---
+++ lua/plugins/sqlite.lua
@@ -0,0 +1,1 @@
+return { "kkharji/sqlite.lua" }
