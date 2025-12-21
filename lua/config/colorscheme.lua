57ca93bcecfdf41b21d7e0402bde8305b9bdbd5c
whale4rain <panyucheng0123@icloud.com>
2025-12-21 10:43:57 +0800

    refactor colorscheme configuration to use blue-whale theme and remove mellifluous setup

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,12 +1,18 @@
 return {
+	-- {
+	-- 	"ramojus/mellifluous.nvim",
+	-- 	-- version = "v0.*", -- uncomment for stable config (some features might be missed if/when v1 comes out)
+	-- 	config = function()
+	-- 		require("mellifluous").setup({}) -- optional, see configuration section.
+	-- 		vim.cmd("colorscheme mellifluous")
+	-- 	end,
+	-- },
 	{
-		"ramojus/mellifluous.nvim",
-		-- version = "v0.*", -- uncomment for stable config (some features might be missed if/when v1 comes out)
+		"whale4rain/blue-whale.nvim",
 		config = function()
-			require("mellifluous").setup({}) -- optional, see configuration section.
-			vim.cmd("colorscheme mellifluous")
+			vim.cmd("colorscheme blue-whale")
 		end,
-	},
+	}
 }
 --	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
